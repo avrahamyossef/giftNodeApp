9f1076872614e8c6be2263c16197789c42fafa86
avraham yossef <avrahamyossef3@gmail.com>
2018-03-28 20:47:39 +0300

    remove update hapi js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const Boom = require('boom');
 const server = new Hapi.Server();
 server.connection({
   host: 'localhost',
-  port: (process.env.port || 8080)
+  port: (process.env.port || 3000)
 });
 //const server = new Hapi.Server();
 
