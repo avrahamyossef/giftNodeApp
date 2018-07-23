2783c4b3e623585abb332c839237c1ece22c633e
avraham yossef <avrahamyossef3@gmail.com>
2018-07-23 16:45:29 +0300

    test2/...

--- Server/config.js
+++ Server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // 1. MongoDB
-    MONGO_URI: "mongodb+srv://giftmongoddb-mpcwb.mongodb.net/test",
+    MONGO_URI: "mongodb://giftmongoddb-shard-00-00-mpcwb.mongodb.net:27017,giftmongoddb-shard-00-01-mpcwb.mongodb.net:27017,giftmongoddb-shard-00-02-mpcwb.mongodb.net:27017/test?replicaSet=GiftMongodDb-shard-0 --ssl --authenticationDatabase admin --username avrahamyossef3 --password <Ay748596>",
 
     // 2. JWT
     TOKEN_SECRET: process.env.TOKEN_SECRET || 'pvpnCCZfwOF85pBjbOebZiYIDhZ3w9LZrKwBZ7152K89mPCOHtbRlmr5Z91ci4L',
