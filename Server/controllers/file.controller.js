213c61581d81db81c496a78859efb66e112e27f5
avraham yossef <avrahamyossef3@gmail.com>
2018-11-25 21:50:39 +0200

    test

--- Server/controllers/file.controller.js
+++ Server/controllers/file.controller.js
@@ -3,18 +3,18 @@ const uploadFolder = "/var/www/html/uploads/";
 const fs = require('fs');
 
 exports.uploadFile = (req, res) => {
-	//res.send('File uploaded successfully! -> filename = ' + req.file.filename);
-	if (err) {
-		res.status(404).json({
-			IsOk: false,
-			errorMessage: 'Error uploading file.',
-			error: err
-		});
-	}
-	res.status(200).json({
-		IsOk: true,
-		Results: "File is uploaded: " + req.file.filename
-	});
+	res.send('File uploaded successfully! -> filename = ' + req.file.filename);
+	// if (err) {
+	// 	res.status(404).json({
+	// 		IsOk: false,
+	// 		errorMessage: 'Error uploading file.',
+	// 		error: err
+	// 	});
+	// }
+	// res.status(200).json({
+	// 	IsOk: true,
+	// 	Results: "File is uploaded: " + req.file.filename
+	// });
 }
 
 exports.listUrlFiles = (req, res) => {
