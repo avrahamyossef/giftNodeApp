9823314a878e5493c9869a6ea3e91879a485a52e
avraham yossef <avrahamyossef3@gmail.com>
2018-11-27 19:13:47 +0200

    test

--- Server/multer.config.js
+++ Server/multer.config.js
@@ -14,7 +14,7 @@ var storage =   multer.diskStorage({
     callback(null, '/var/www/html/uploads/');
   },
   filename: function (req, file, callback) {
-    callback(null, file.originalname + '-' + Date.now());
+    callback(null, file.originalname);
   }
 });
  
