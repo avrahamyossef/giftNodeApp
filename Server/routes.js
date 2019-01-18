7ca6c3f01e8a2283d18592a63785ded52c246a52
avraham yossef <avrahamyossef3@gmail.com>
2019-01-19 00:44:47 +0200

    get

--- Server/routes.js
+++ Server/routes.js
@@ -45,5 +45,5 @@ module.exports = function (app) {
     //dna club api
     app.post('/auth/dnaRegisterUser', Auth.registerDnaUsers);
     app.post('/auth/updateUserResults', Auth.updateUserResults);
-    app.post('/auth/getQuizResults', Auth.getQuizResults);
+    app.get('/auth/getQuizResults', Auth.getQuizResults);
 };  
