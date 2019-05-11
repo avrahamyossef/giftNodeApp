6b934dfce2e8cf5607342b50cbbb66674eb6042d
avraham yossef <avrahamyossef3@gmail.com>
2019-05-11 19:00:51 +0300

    zx`

--- Server/models/blackSummerEvents.js
+++ Server/models/blackSummerEvents.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const EventsModel = new Schema({
-    orderId : { type: Number, required: false },
+    orderId: { type: Number, required: false },
     name: { type: String, required: false },
     date: { type: Date, required: false },
     description: { type: Number, required: false },
-    image: { type: Number, required: false },
-    video: { type: Number, required: false },
+    image: { type: String, required: false },
+    video: { type: String, required: false },
     isClose: { type: Boolean, required: false },
     isSpecial: { type: Boolean, required: false }
 });
